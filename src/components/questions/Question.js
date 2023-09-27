5c0a9e241cbbc24718b42460473c7bc24ad5076e
portafolio <edgar.cortes1812@gmail.com>
2023-09-26 21:20:34 -0600

    Se agrego seccion encuesta diseño

--- src/components/questions/Question.js
+++ src/components/questions/Question.js
@@ -2,7 +2,33 @@ import React from 'react'
 import './Questions.css'
 
 export default function Question() {
-  return (
-    <div>Question</div>
-  )
+    return (
+        <div className='bg-encuesta'>
+            <div className='container con-position d-flex justify-content-center'>
+                <div className='row con-ins p-3'>
+                    <div className='col-md-4 text-center pt-3'>
+                        <img src="/img/logo-kinder.png" alt="" />
+                    </div>
+                    <div className='col-md-8 p-2'>
+                        <div className='text-center'>
+                            <h1 className='text-center'>Instrucciones</h1>
+                        </div>
+                        <p className='pt-4'>
+                            Esperamos que estén bien. Queremos mejorar
+                            la experiencia de sus hijos en el jardín de
+                            niños. Por favor, participen en nuestra encuesta
+                            en línea para proporcionarnos sus valiosas opiniones
+                            y sugerencias. Sus comentarios son esenciales para nosotros.
+                        </p>
+                        <p className='pt-4'>
+                            1.- Responda a las preguntas de la encuesta de manera sincera y objetiva.<br />
+                            2.- Asegúrese de completar todas las preguntas obligatorias marcadas con un asterisco (*).<br />
+                            3.- Puede proporcionar comentarios adicionales en la sección de comentarios si lo desea.<br />
+                            4.- Haga clic en el botón "Enviar" al final de la encuesta para registrar sus respuestas.<br />
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
