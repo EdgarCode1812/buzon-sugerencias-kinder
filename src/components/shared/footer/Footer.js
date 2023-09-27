2c8694c518b5f4dfde0bc54e939bf6af400b9a9a
portafolio <edgar.cortes1812@gmail.com>
2023-09-27 13:20:28 -0600

    Se agregaron cambios al footer

--- src/components/shared/footer/Footer.js
+++ src/components/shared/footer/Footer.js
@@ -8,19 +8,32 @@ export default function Footer() {
 
                 <div class="col-md-8 text-center">
                     <div class="p-2 mt-3">
-                        <span className='pe-1'> Todos los Derechos Reservados © 2023 | Community Technology | Desarrollado por: </span><a href='https://binascode.com/' target='_blank' rel="noreferrer"> www.binascode.com</a>
+                        <span className='pe-1'> Todos los Derechos Reservados © 2023 | Community Technology | Desarrollado por BinasCode </span>
                     </div>
                 </div>
 
                 <div class="col-md-4 ">
-                <div class="p-2 text-center d-flex justify-content-center mt-1">
-                    <div class="p-2">
-                        <img src="/img/facebook.png" width={30} alt="" />
+                    <div class="row p-2 text-center mt-1">
+
+                        <div className='col-md-6 d-flex justify-content-center'>
+                            <div class="p-2">
+                                <img src="/img/logoBinasCode.png" width={30} alt="" />
+                            </div>
+                            <div class="p-2">
+                                <a href='https://binascode.com/' target='_blank' rel="noreferrer"> www.binascode.com</a>
+                            </div>
+                        </div>
+
+                        <div className='col-md-6 d-flex justify-content-center'>
+                            <div class="p-2">
+                                <img src="/img/facebook.png" width={30} alt="" />
+                            </div>
+                            <div class="p-2">
+                                <a href='https://www.facebook.com/profile.php?id=100092163859160' target='_blank' rel="noreferrer">/BinasCode</a>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="p-2">
-                        <a href='https://www.facebook.com/profile.php?id=100092163859160' target='_blank' rel="noreferrer">/BinasCode</a>
-                    </div>
-                </div>
                 </div>
 
             </div>
