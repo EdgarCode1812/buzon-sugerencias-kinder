5d47edf454f1c694109588c436ecb251d40b0780
portafolio <edgar.cortes1812@gmail.com>
2023-09-27 10:33:23 -0600

    Se agrego un cargando para el inicio

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,31 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'
+import './Home.css';
 
 export default function Home() {
+    const [videoLoaded, setVideoLoaded] = useState(false);
+
+    useEffect(() => {
+        const videoElement = document.querySelector('video');
+
+        if (videoElement) {
+            videoElement.addEventListener('loadeddata', () => {
+                setVideoLoaded(true);
+                console.log("El video se ha cargado por completo");
+            });
+        }
+    }, []);
+
+    const videoComponent = (
+        <div className="brightness">
+            <video autoPlay={true} loop={true} muted={true} src="/video/video-kinder.mp4"></video>
+        </div>
+    );
+
     return (
         <div className="bg-video-wrap">
-            <div className="brightness">
-                <video autoPlay={true} loop={true} muted={true} src="/video/video-kinder.mp4">
-                </video>
-            </div>
-
-            <div className="overlay">
-            </div>
-            <div className="center-div text-center">
-                <div className="margin-top">
-                    <div className=''>
-                        <img src="/img/logo-kinder.png" alt="" />
+            {videoLoaded ? (
+                <>
+                    {videoComponent}
+                    <div className="overlay"></div>
+                    <div className="center-div text-center">
+                        <div className="margin-top">
+                            <div className=''>
+                                <img src="/img/logo-kinder.png" alt="" />
+                            </div>
+                            <div className='text-wel'>
+                                <p className="p-2">Bienvenido al buzón de sugerencias</p>
+                            </div>
+                            <div className='btn-start'>
+                                <Link to="/Encuesta" className="btn btn-primary btn-lg pt-2 pb-2 ps-4 pe-4"> Iniciar </Link>
+                            </div>
+                        </div>
                     </div>
-                    <div className='text-wel'>
-                        <p className="p-2">Bienvenido al buzón de sugerencias</p>
+                </>
+            ) : (
+                <>
+                    {videoComponent}
+                    <div className="overlay"></div>
+                    <div className="center-div text-center">
+                        <div className="margin-top">
+                            <div className=''>
+                                <img src="/img/logo-kinder.png" className='img-cargando' alt="" />
+                            </div>
+                            <div className='text-wel'>
+                                <p className="p-2">Cargando...</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='btn-start'>
-                        <Link to="/Encuesta" className="btn btn-primary btn-lg pt-2 pb-2 ps-4 pe-4"> Iniciar </Link>
-                    </div>
-
-                </div>
-            </div>
+                </>
+            )}
         </div>
-    )
+    );
 }
